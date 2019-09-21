e21325d819863978ea523f2e2e47dab8501aba19
Xennis <code@xennis.org>
2019-09-22 00:44:37 +0200

    Apps | Add Google login

--- apps/its-12-oclock/lib/main.dart
+++ apps/its-12-oclock/lib/main.dart
@@ -1,9 +1,6 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:its_12_oclock/placefinder.dart';
+
+import 'view/login/login_page.dart';
 
 void main() => runApp(App());
 
@@ -15,97 +12,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: HomePage(title: 'It\'s 12 o\'clock'),
+      home: LoginPage(),
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  Position position;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () {
-                _findPlaces();
-                setState(() {});
-              },
-              color: Theme.of(context).primaryColor,
-              textColor: Colors.white,
-              child: Text("Save restaurant"),
-            ),
-            _placesWidget(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _placesWidget() {
-    return FutureBuilder<List<Place>>(
-        future: _findPlaces(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              shrinkWrap: true,
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index) {
-                Place place = snapshot.data[index];
-                // await Geolocator().distanceBetween(position.latitude, position.longitude, place.location.lat, place.location.lng);
-                return Column(children: <Widget>[
-                  Card(
-                    child: ListTile(
-                      leading: FlutterLogo(size: 56.0),
-                      title: Text(place.name),
-                      subtitle: Text("Distance: ${place.distance}m, Rating: ${place.rating}"),
-                      trailing: Icon(Icons.more_vert),
-                    ),
-                  )
-                ]);
-              },
-            );
-          } else if (snapshot.hasError) {
-            log(snapshot.error);
-            return Text("${snapshot.error}");
-          }
-          return CircularProgressIndicator();
-        });
-  }
-
-  Future<List<Place>> _findPlaces() async {
-    try {
-      position = await Geolocator()
-          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      return PlaceFinder.find(
-          Location(lat: position.latitude, lng: position.longitude));
-    } on PlatformException catch (e) {
-      if (e.code == 'PERMISSION_DENIED') {
-        // TODO: Handle error
-        print(e);
-        throw e;
-      } else {
-        // TODO: Handle error
-        print(e);
-        throw e;
-      }
-    }
   }
 }
---
+++ apps/its-12-oclock/lib/view/login/sign_in.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+String name;
+
+Future<bool> signInWithGoogle() async {
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+
+  final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+
+  assert(user.displayName != null);
+  name = user.displayName;
+  name = name.split(" ")[0];
+
+  assert(!user.isAnonymous);
+  assert(await user.getIdToken() != null);
+
+  final FirebaseUser currentUser = await _auth.currentUser();
+  assert(user.uid == currentUser.uid);
+
+  return true;
+}
+
