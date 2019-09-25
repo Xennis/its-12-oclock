7ffd33f3b932c581b140f77366e60e408ee185c0
Xennis <code@xennis.org>
2019-09-25 15:57:24 +0200

    Apps | Show home page for logged in users

--- apps/its-12-oclock/lib/main.dart
+++ apps/its-12-oclock/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import 'view/home/home_page.dart';
 import 'view/login/login_page.dart';
 
 void main() => runApp(App());
@@ -12,7 +14,19 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: LoginPage(),
+      home: _firstPage(),
+    );
+  }
+
+  Widget _firstPage() {
+    return FutureBuilder<FirebaseUser>(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
+          if (snapshot.hasData) {
+            return HomePage(title: 'It\'s 12 o\'clock');
+          }
+          return LoginPage();
+      },
     );
   }
 }
