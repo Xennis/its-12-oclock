4481a66f5ea3c2630d0ff18348c525ffd77a7b08
Xennis <code@xennis.org>
2019-10-25 17:47:52 +0200

    Apps | Create dismissble list

--- apps/its-12-oclock/lib/screens/main/home.dart
+++ apps/its-12-oclock/lib/screens/main/home.dart
@@ -1,5 +1,13 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:its_12_oclock/models/location.dart';
+import 'package:its_12_oclock/models/place.dart';
+import 'package:its_12_oclock/services/history.dart';
+import 'package:its_12_oclock/services/placefinder.dart';
 import 'package:its_12_oclock/services/sign_in.dart';
 
 class HomeWidget extends StatefulWidget {
@@ -19,12 +27,110 @@ class _HomeWidgetState extends State<HomeWidget> {
           children: <Widget>[
             SizedBox(height: 15),
             Text(
-              "Hey ${fbUser.displayName.split(" ")[0]}",
-              style: TextStyle(fontSize: 25),
+              "Hey ${fbUser.displayName.split(" ")[0]}, I recommend you",
+              style: TextStyle(fontSize: 20),
             ),
+            _placesWidget(),
           ],
         ),
       ),
     );
+  }
+
+  String _placeAbbr(String name) {
+    List<String> split = name.split(" ");
+    if (split.length == 0) {
+      return "";
+    } else if (split.length == 1) {
+      return split[0][0];
+    }
+    return split[0][0] + split[1][0];
+  }
+
+  Widget _placesWidget() {
+    return FutureBuilder<List<Place>>(
+        future: _findPlaces(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              shrinkWrap: true,
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, int index) {
+                Place place = snapshot.data[index];
+                return Dismissible(
+                    key: Key(index.toString()),
+                    background: Container(
+                      alignment: AlignmentDirectional.centerStart,
+                      color: Colors.green,
+                      child: Padding(
+                          padding: EdgeInsets.fromLTRB(20.0, 0, 0, 0),
+                          child: Icon(Icons.thumb_up, color: Colors.white)),
+                    ),
+                    secondaryBackground: Container(
+                      alignment: AlignmentDirectional.centerEnd,
+                      color: Colors.red,
+                      child: Padding(
+                          padding: EdgeInsets.fromLTRB(0.0, 0.0, 20.0, 0),
+                          child: Icon(Icons.thumb_down, color: Colors.white)),
+                    ),
+                    onDismissed: (DismissDirection direction) {
+                      //setState(() {
+                      //  snapshot.data.removeAt(index);
+                      //});
+                      if (direction == DismissDirection.startToEnd) {
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(content: Text("Liked ${place.name}")));
+                      } else {
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(content: Text("Disliked ${place.name}")));
+                      }
+                    },
+                    child: Column(children: <Widget>[
+                      Card(
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            child: Text(_placeAbbr(place.name)),
+                            backgroundColor: Theme.of(context).primaryColor,
+                          ),
+                          onTap: () {
+                            History.save(fbUser, place);
+                          },
+                          title: Text(place.name),
+                          subtitle: Text(
+                              "Distance: ${place.distance}m, Rating: ${place.rating}"),
+                        ),
+                      )
+                    ]));
+              },
+            );
+          } else if (snapshot.hasError) {
+            log(snapshot.error);
+            return Text("${snapshot.error}");
+          }
+          return CircularProgressIndicator();
+        });
+  }
+
+  Future<List<Place>> _findPlaces() async {
+    try {
+      FirebaseUser user = await FirebaseAuth.instance.currentUser();
+      IdTokenResult tokenResult = await user.getIdToken(refresh: true);
+
+      position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      return PlaceFinder.find(
+          Location(lat: position.latitude, lng: position.longitude),
+          tokenResult.token);
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        // TODO: Handle error
+        print(e);
+        throw e;
+      } else {
+        // TODO: Handle error
+        print(e);
+        throw e;
+      }
+    }
   }
 }
